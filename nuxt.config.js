004e46b02b3fafe128f36c0d52517ffe475716af
Irina Kvitkina <irina.kvitkina@gmail.com>
2020-06-19 12:48:35 +0300

    удаление лишнего кода

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,14 +25,7 @@ module.exports = {
     ** Run ESLint on save
     */
     extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
+      
     }
   }
 }
