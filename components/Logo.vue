4c67c150b69f622f50b2cbb0f3331a9de9143a85
Roman Didukh <newrdlink@yandex.ru>
2020-06-21 12:38:23 +0300

    сделать клик по лого в шапке

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="logo">
-    <img class="logo__image" src="@/assets/images/logo.svg" />
+    <a href="#" class="logo__link">
+      <img class="logo__image" src="@/assets/images/logo.svg" />
+    </a>
   </div>
 </template>
 
@@ -8,5 +10,13 @@
 
 <style scoped>
 .logo {
+}
+.logo__link {
+}
+.logo__image {
+  transition: ease-in 0.2s transform;
+}
+.logo__image:hover {
+  transform: rotate(-260deg);
 }
 </style>
