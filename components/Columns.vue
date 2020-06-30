84209b8eabbc1f6606880375c669db12d018cc5d
George Chetyrin <klon84@gmail.com>
2020-06-30 23:59:52 +0300

    Правки в колонки для задач

--- components/Columns.vue
+++ components/Columns.vue
@@ -1,44 +1,31 @@
 <template>
   <div class="columns">
-    <div class="column__task">
-      <div class="column__scroll"></div>
-    </div>
-    <div class="column__task">
-      <div class="column__scroll"></div>
-    </div>
-    <div class="column__task">
-      <div class="column__scroll"></div>
-    </div>
+    <div class="column__task"></div>
+    <div class="column__task"></div>
+    <div class="column__task"></div>
   </div>
 </template>
 
 <script></script>
 
-<style>
+
+<style scoped>
 .columns {
   display: flex;
-  margin: 87px auto 80px;
-  justify-content: center;
+  margin: 0 auto;
+  max-width: 100%;
+  justify-content: space-between;
   box-sizing: border-box;
   padding: 0 20px;
-}
-
-.column__scroll {
-  width: 9px;
-  height: 65px;
-  background: #818181;
-  border-radius: 5px;
-  margin: 12px 5px 0 0;
 }
 
 .column__task {
   width: 340px;
   height: 536px;
   background: #c4c4c4;
-  margin-right: 12px;
+  margin: 0;
+  overflow: scroll;
+  overflow-x: hidden;
 }
 
-.column__task:nth-child(3) {
-  margin-right: 0;
-}
 </style>
