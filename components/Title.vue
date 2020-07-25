97e1ee7e8ac8377c2dc5c4444f9b3c4b6732f707
Irina Kvitkina <irina.kvitkina@gmail.com>
2020-07-25 13:11:32 +0300

    добавление заголовков

---
+++ components/Title.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2 class="title">{{title}}</h2>
+</template>
+
+<script>
+  export default {
+    data () {
+       return {
+         title: 'Backlog'
+       }  
+    }
+  }
+</script>
+
+<style>
+  .title{
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 33px;
+    margin-bottom: 20px;
+  }
+</style>
